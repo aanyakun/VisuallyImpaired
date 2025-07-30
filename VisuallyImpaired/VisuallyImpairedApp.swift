5a2cd90ade8e7d93e87d6ae1e29e6f1e2abee72c
Aanya Kungwani <aanyak820@gmail.com>
2025-07-30 16:23:34 -0400

    Initial Commit

---
+++ VisuallyImpaired/VisuallyImpairedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisuallyImpairedApp.swift
+//  VisuallyImpaired
+//
+//  Created by aanya kungwani on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct VisuallyImpairedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
